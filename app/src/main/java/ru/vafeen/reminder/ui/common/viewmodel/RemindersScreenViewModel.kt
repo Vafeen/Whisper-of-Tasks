7863ab08334ed877c4946ae3312096ffb9abe158
Vafeen <666av6@gmail.com>
2024-08-01 17:19:08 +0300

    Add viewModel classes

---
+++ app/src/main/java/ru/vafeen/reminder/ui/common/viewmodel/RemindersScreenViewModel.kt
@@ -0,0 +1,10 @@
+package ru.vafeen.reminder.ui.common.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RemindersScreenViewModel @Inject constructor() : ViewModel() {
+
+}
