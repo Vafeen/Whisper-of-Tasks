3cea826779833b22a9a6d4f5a1e1737b69fbef35
Vafeen <666av6@gmail.com>
2024-08-01 17:44:03 +0300

    Add ViewModelsFactoryProvider

--- app/src/main/java/ru/vafeen/reminder/main/MainActivity.kt
+++ app/src/main/java/ru/vafeen/reminder/main/MainActivity.kt
@@ -12,34 +12,29 @@ import dagger.hilt.android.AndroidEntryPoint
 import ru.vafeen.reminder.ui.common.navigation.ScreenRoute
 import ru.vafeen.reminder.ui.common.screen.MainScreen
 import ru.vafeen.reminder.ui.common.screen.RemindersScreen
-import ru.vafeen.reminder.ui.common.viewmodel.MainScreenViewModel
-import ru.vafeen.reminder.ui.common.viewmodel.RemindersScreenViewModel
 import ru.vafeen.reminder.ui.common.viewmodel.factory.MainScreenViewModelFactory
 import ru.vafeen.reminder.ui.common.viewmodel.factory.ReminderScreenViewModelFactory
+import ru.vafeen.reminder.ui.common.viewmodel.factory.provider.ViewModelsFactoryProvider
 import ru.vafeen.reminder.ui.theme.ReminderTheme
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @Inject
-    lateinit var mainScreenViewModelFactory: MainScreenViewModelFactory
-
-    @Inject
-    lateinit var reminderScreenViewModelFactory: ReminderScreenViewModelFactory
+    lateinit var viewModelsFactoryProvider: ViewModelsFactoryProvider
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ReminderTheme {
-
                 val navController = rememberNavController()
                 NavHost(navController = navController, startDestination = ScreenRoute.Main.route) {
                     composable(route = ScreenRoute.Main.route) {
-                        MainScreen(viewModel = viewModel(factory = mainScreenViewModelFactory))
+                        MainScreen(viewModel = viewModel(factory = viewModelsFactoryProvider.mainScreenViewModelFactory))
                     }
                     composable(route = ScreenRoute.Reminders.route) {
-                        RemindersScreen(viewModel = viewModel(factory = reminderScreenViewModelFactory))
+                        RemindersScreen(viewModel = viewModel(factory = viewModelsFactoryProvider.reminderScreenViewModelFactory))
                     }
                 }
             }
