8c9382cafda39ad8928d07c8e33954a5f0018ed4
Vafeen <666av6@gmail.com>
2024-08-01 18:06:21 +0300

    Add simple layouts of screens

--- app/src/main/java/ru/vafeen/reminder/ui/common/screen/MainScreen.kt
+++ app/src/main/java/ru/vafeen/reminder/ui/common/screen/MainScreen.kt
@@ -3,22 +3,43 @@ package ru.vafeen.reminder.ui.common.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.traceEventEnd
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Screen
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import ru.vafeen.reminder.noui.viewmodel.MainScreenViewModel
+import ru.vafeen.reminder.ui.common.components.BottomBar
+import ru.vafeen.reminder.ui.common.navigation.ScreenRoute
 
 @Composable
 fun MainScreen(
     viewModel: MainScreenViewModel,
     navController: NavController
 ) {
-    Column(
+    Scaffold(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "ads")
+        bottomBar = {
+            BottomBar(
+                containerColor = Color.Red,
+                selectedMainScreen = true,
+                navigateToRemindersScreen = { navController.navigate(ScreenRoute.Reminders.route) })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "main")
+        }
     }
+
 }
--- app/src/main/java/ru/vafeen/reminder/ui/common/screen/RemindersScreen.kt
+++ app/src/main/java/ru/vafeen/reminder/ui/common/screen/RemindersScreen.kt
@@ -1,13 +1,46 @@
 package ru.vafeen.reminder.ui.common.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import ru.vafeen.reminder.noui.viewmodel.RemindersScreenViewModel
+import ru.vafeen.reminder.ui.common.components.BottomBar
+import ru.vafeen.reminder.ui.common.navigation.ScreenRoute
 
 @Composable
 fun RemindersScreen(
     viewModel: RemindersScreenViewModel,
     navController: NavController
 ) {
-
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            BottomBar(
+                containerColor = Color.Red,
+                selectedRemindersScreen = true,
+                navigateToMainScreen = {
+                    navController.popBackStack()
+                    navController.popBackStack()
+                    navController.navigate(ScreenRoute.Main.route)
+                })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "reminders")
+        }
+    }
 }
