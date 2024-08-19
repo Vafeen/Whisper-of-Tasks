fc45216ebaf91827256523e8a13bfb63311bc98c
Vafeen <666av6@gmail.com>
2024-08-19 10:38:00 +0300

    Add Font

---
+++ app/src/main/java/ru/vafeen/reminder/ui/theme/Font.kt
@@ -0,0 +1,10 @@
+package ru.vafeen.reminder.ui.theme
+
+import androidx.compose.ui.unit.sp
+
+object FontSize {
+    val small = 10.sp
+    val medium = 15.sp
+    val big = 18.sp
+    val huge = 22.sp
+}
