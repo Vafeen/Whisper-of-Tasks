5156c8f1fe9740d21b4589da244d600391a436d2
Vafeen <666av6@gmail.com>
2024-08-19 14:00:43 +0300

    Add DefaultDialog

---
+++ app/src/main/java/ru/vafeen/reminder/ui/common/components/DefaultDialog.kt
@@ -0,0 +1,19 @@
+package ru.vafeen.reminder.ui.common.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import ru.vafeen.reminder.ui.theme.Theme
+
+@Composable
+fun DefaultDialog(
+    onDismissRequest: () -> Unit, content: @Composable (ColumnScope.(Dp) -> Unit)
+) {
+    Dialog(onDismissRequest = onDismissRequest) {
+        Card(border = BorderStroke(width = 2.dp, Theme.colors.oppositeTheme)) { content(10.dp) }
+    }
+}
