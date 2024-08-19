37d5e945ce7f412ab50d9e6225a04120a9076bb9
Vafeen <666av6@gmail.com>
2024-08-19 14:36:37 +0300

    Rename fun for inserting

--- app/src/main/java/ru/vafeen/reminder/noui/local_database/DatabaseRepository.kt
+++ app/src/main/java/ru/vafeen/reminder/noui/local_database/DatabaseRepository.kt
@@ -9,6 +9,6 @@ class DatabaseRepository @Inject constructor(private val db: AppDatabase) {
     private val reminderDao = db.reminderDao()
 
     fun getAllReminders(): Flow<List<Reminder>> = reminderDao.getAllAsFlow()
-    suspend fun insertAllReminders(vararg reminder: Reminder) = reminderDao.insertAll(*reminder)
+    suspend fun insertReminder(vararg reminder: Reminder) = reminderDao.insertAll(*reminder)
     suspend fun removeReminder(vararg reminder: Reminder) = reminderDao.delete(*reminder)
 }
