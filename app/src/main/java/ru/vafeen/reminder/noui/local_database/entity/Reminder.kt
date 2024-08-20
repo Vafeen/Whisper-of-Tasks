40ed3678a4a5bcbfc25321eeaadca4c4777a7cde
Vafeen <666av6@gmail.com>
2024-08-20 11:01:52 +0300

    Modify Reminder

--- app/src/main/java/ru/vafeen/reminder/noui/local_database/entity/Reminder.kt
+++ app/src/main/java/ru/vafeen/reminder/noui/local_database/entity/Reminder.kt
@@ -2,6 +2,7 @@ package ru.vafeen.reminder.noui.local_database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import ru.vafeen.reminder.noui.RepeatDuration
 import java.time.LocalDateTime
 
 @Entity
@@ -9,7 +10,8 @@ data class Reminder(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
     val text: String,
-    val dt: LocalDateTime
+    val dt: LocalDateTime,
+    val repeatDuration: RepeatDuration = RepeatDuration.NoRepeat
 ) {
     override fun toString(): String = "$id $title $text"
 }
