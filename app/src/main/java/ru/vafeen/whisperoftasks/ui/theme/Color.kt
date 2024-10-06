3e427a3c324d15b3fc8aa4a79bf700960dc88690
Vafeen <666av6@gmail.com>
2024-10-07 02:34:51 +0300

    Modify mainLightColor

--- app/src/main/java/ru/vafeen/whisperoftasks/ui/theme/Color.kt
+++ app/src/main/java/ru/vafeen/whisperoftasks/ui/theme/Color.kt
@@ -2,6 +2,6 @@ package ru.vafeen.whisperoftasks.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val mainLightColor = Color(0xFFE33722)
+val mainLightColor = Color(0xFF00C3AB)
 val mainDarkColor = Color(0xFF430FBF)
 val updateAvailableColor = Color(0xFF0079d6)
