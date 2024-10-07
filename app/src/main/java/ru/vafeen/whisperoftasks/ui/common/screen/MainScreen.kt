5669654aa2042bc152273910a4aca9c5288c0f14
Vafeen <666av6@gmail.com>
2024-10-07 23:40:23 +0300

    Add vertical scroll to MainScreen.kt

--- app/src/main/java/ru/vafeen/whisperoftasks/ui/common/screen/MainScreen.kt
+++ app/src/main/java/ru/vafeen/whisperoftasks/ui/common/screen/MainScreen.kt
@@ -17,6 +17,8 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -240,6 +242,7 @@ fun MainScreen(
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
+                        .verticalScroll(rememberScrollState())
                 ) {
                     val dateOfThisPage = viewModel.todayDate.plusDays(page.toLong())
                     localDate = viewModel.todayDate.plusDays(pagerState.currentPage.toLong())
