cb7ef72118a662461b7777fefbae88bca0fb6136
Vafeen <666av6@gmail.com>
2024-10-07 23:38:34 +0300

    Modify notification icon

--- app/src/main/java/ru/vafeen/whisperoftasks/noui/notification/NotificationService.kt
+++ app/src/main/java/ru/vafeen/whisperoftasks/noui/notification/NotificationService.kt
@@ -28,7 +28,7 @@ class NotificationService(
     ) {
         val notification =
             NotificationCompat.Builder(context, NotificationChannelInfo.NOTIFICATION_CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setSmallIcon(R.drawable.message)
                 .setContentTitle(title)
                 .setContentText(text)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
