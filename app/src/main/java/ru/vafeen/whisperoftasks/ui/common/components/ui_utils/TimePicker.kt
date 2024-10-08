d0353e45175caa88c5fe4c91b78504c1fcbf38c7
Vafeen <666av6@gmail.com>
2024-10-08 14:23:00 +0300

    Modify border in TimePicker.kt

--- app/src/main/java/ru/vafeen/whisperoftasks/ui/common/components/ui_utils/TimePicker.kt
+++ app/src/main/java/ru/vafeen/whisperoftasks/ui/common/components/ui_utils/TimePicker.kt
@@ -1,7 +1,6 @@
 package ru.vafeen.whisperoftasks.ui.common.components.ui_utils
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -23,7 +21,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -36,16 +38,30 @@ import java.time.LocalTime
 
 
 @Composable
-fun Border(itemHeight: Dp) {
+fun Border(itemHeight: Dp, color: Color) {
+    val width = 2.dp
+    val strokeWidthPx = with(LocalDensity.current) { width.toPx() }
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .height(itemHeight)
-            .background(Theme.colors.oppositeTheme)
-            .padding(top = 2.dp, bottom = 2.dp)
-            .background(Theme.colors.singleTheme)
+            .drawBehind {
+                drawLine(
+                    color = color,
+                    strokeWidth = strokeWidthPx,
+                    start = Offset(0f, 0f),
+                    end = Offset(size.width, 0f) // Верхняя граница
+                )
+                drawLine(
+                    color = color,
+                    strokeWidth = strokeWidthPx,
+                    start = Offset(0f, size.height),
+                    end = Offset(size.width, size.height) // Нижняя граница
+                )
+            }
     ) {}
 }
+
 
 @Composable
 fun MyDateTimePicker(
@@ -133,7 +149,7 @@ private fun DateColumnPicker(
         modifier = modifier.height(listHeight), contentAlignment = Alignment.Center
     ) {
 
-        Border(itemHeight = itemHeight)
+        Border(itemHeight = itemHeight, color = Theme.colors.oppositeTheme)
 
 
         LazyColumn(
@@ -191,7 +207,7 @@ private fun TimeColumnPicker(
     Box(
         modifier = modifier.height(listHeight), contentAlignment = Alignment.Center
     ) {
-        Border(itemHeight = itemHeight)
+        Border(itemHeight = itemHeight, color = Theme.colors.oppositeTheme)
 
         LazyColumn(
             state = listState,
