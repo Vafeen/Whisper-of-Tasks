68eca687d5dea2f0ffbd0328a6d5a7c929ed7d4f
Vafeen <666av6@gmail.com>
2024-10-14 18:10:22 +0300

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "ru.vafeen.whisperoftasks"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.0-rc11"
+        versionCode = 12
+        versionName = "1.0-rc12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
