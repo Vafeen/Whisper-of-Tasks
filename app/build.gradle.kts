0373c622fbe47731271cd8a5c774746afaccaabd
Vafeen <666av6@gmail.com>
2024-12-28 18:08:09 +0300

    New version: 1.1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.vafeen.whisperoftasks"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "1.1.7"
+        versionCode = 30
+        versionName = "1.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
