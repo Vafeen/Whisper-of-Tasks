74aeb801f3bcb676e1a0a74500be260d999394ca
Vafeen <666av6@gmail.com>
2025-01-03 23:31:10 +0300

    Add MainActivityViewModel to MainActivity

--- presentation/src/main/java/ru/vafeen/whisperoftasks/presentation/main/MainActivity.kt
+++ presentation/src/main/java/ru/vafeen/whisperoftasks/presentation/main/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import ru.vafeen.whisperoftasks.presentation.common.navigation.NavRoot
 import ru.vafeen.whisperoftasks.presentation.ui.theme.ReminderTheme
 
 internal class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainActivityViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
