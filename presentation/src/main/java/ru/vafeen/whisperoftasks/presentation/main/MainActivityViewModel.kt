ed0713ddf399051e962d5f7630019b5e740eb299
Vafeen <666av6@gmail.com>
2025-01-03 23:24:40 +0300

    Add MainActivityViewModel

---
+++ presentation/src/main/java/ru/vafeen/whisperoftasks/presentation/main/MainActivityViewModel.kt
@@ -0,0 +1,12 @@
+package ru.vafeen.whisperoftasks.presentation.main
+
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavHostController
+import kotlinx.coroutines.flow.StateFlow
+import ru.vafeen.whisperoftasks.presentation.common.navigation.BottomBarNavigator
+import ru.vafeen.whisperoftasks.presentation.common.navigation.Screen
+
+internal class MainActivityViewModel(
+) : ViewModel() {
+
+}
