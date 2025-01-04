f56e763207ea04199340ef16da62e3c7ea2f0eec
Vafeen <666av6@gmail.com>
2025-01-04 18:49:24 +0300

    Add background

--- presentation/src/main/java/ru/vafeen/whisperoftasks/presentation/components/navigation/NavRoot.kt
+++ presentation/src/main/java/ru/vafeen/whisperoftasks/presentation/components/navigation/NavRoot.kt
@@ -1,5 +1,6 @@
 package ru.vafeen.whisperoftasks.presentation.components.navigation
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -14,12 +15,14 @@ import ru.vafeen.whisperoftasks.presentation.common.components.bottom_bar.Bottom
 import ru.vafeen.whisperoftasks.presentation.components.main_screen.MainScreen
 import ru.vafeen.whisperoftasks.presentation.components.reminders_screen.RemindersScreen
 import ru.vafeen.whisperoftasks.presentation.components.settings_screen.SettingsScreen
+import ru.vafeen.whisperoftasks.presentation.ui.theme.Theme
 
 
 @Composable
 internal fun NavRoot(bottomBarNavigator: BottomBarNavigator) {
     bottomBarNavigator.navController = rememberNavController()
     Scaffold(
+        containerColor = Theme.colors.background,
         bottomBar = {
             val selectedScreen by bottomBarNavigator.currentScreen.collectAsState()
             BottomBar(
@@ -29,13 +32,17 @@ internal fun NavRoot(bottomBarNavigator: BottomBarNavigator) {
             )
         }
     ) { innerPadding ->
-        NavHost(
-            modifier = Modifier.padding(innerPadding),
-            navController = bottomBarNavigator.navController!!, startDestination = Screen.Main
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
         ) {
-            composable<Screen.Main> { MainScreen() }
-            composable<Screen.Reminders> { RemindersScreen() }
-            composable<Screen.Settings> { SettingsScreen() }
+            NavHost(
+                navController = bottomBarNavigator.navController!!, startDestination = Screen.Main
+            ) {
+                composable<Screen.Main> { MainScreen() }
+                composable<Screen.Reminders> { RemindersScreen() }
+                composable<Screen.Settings> { SettingsScreen() }
+            }
         }
     }
 }
