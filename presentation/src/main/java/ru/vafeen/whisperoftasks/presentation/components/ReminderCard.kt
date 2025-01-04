ccd9a62fc3dbf5267aa32db5ec1bc4ffdcad7228
Vafeen <666av6@gmail.com>
2025-01-04 19:00:32 +0300

    Add reminders showing

---
+++ presentation/src/main/java/ru/vafeen/whisperoftasks/presentation/components/ReminderCard.kt
@@ -0,0 +1,29 @@
+package ru.vafeen.whisperoftasks.presentation.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import ru.vafeen.whisperoftasks.domain.domain_models.Reminder
+import ru.vafeen.whisperoftasks.presentation.ui.theme.FontSize
+import ru.vafeen.whisperoftasks.presentation.ui.theme.Theme
+
+
+@Composable
+fun Reminder.ReminderCard() {
+    Card(
+        modifier = Modifier.padding(5.dp),
+        colors = CardDefaults.cardColors(containerColor = Theme.colors.buttonColor)
+    ) {
+        Column(modifier = Modifier.padding(5.dp)) {
+            TextForThisTheme(text = title, fontSize = FontSize.medium19, maxLines = 5)
+            Spacer(modifier = Modifier.height(5.dp))
+            TextForThisTheme(text = text, fontSize = FontSize.small17, maxLines = 5)
+        }
+    }
+}
--- presentation/src/main/java/ru/vafeen/whisperoftasks/presentation/components/reminders_screen/RemindersScreen.kt
+++ presentation/src/main/java/ru/vafeen/whisperoftasks/presentation/components/reminders_screen/RemindersScreen.kt
@@ -1,7 +1,32 @@
 package ru.vafeen.whisperoftasks.presentation.components.reminders_screen
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import org.koin.androidx.compose.koinViewModel
+import ru.vafeen.whisperoftasks.presentation.components.ReminderCard
+import ru.vafeen.whisperoftasks.presentation.ui.theme.Theme
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun RemindersScreen() {
+    val viewModel: RemindersScreenViewModel = koinViewModel()
+    val reminders by viewModel.remindersFlow.collectAsState()
+    Scaffold(
+        containerColor = Theme.colors.background, modifier = Modifier.fillMaxSize()
+    ) {
+        LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+            items(items = reminders) {
+                it.ReminderCard()
+            }
+        }
+
+    }
 }
