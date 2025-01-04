4e00af77fa8e8d6d834ba6da1bfb444dc1531e4a
Vafeen <666av6@gmail.com>
2025-01-04 14:20:55 +0300

    Add ReminderRepository

---
+++ domain/src/main/java/ru/vafeen/whisperoftasks/domain/database/ReminderRepository.kt
@@ -0,0 +1,35 @@
+package ru.vafeen.whisperoftasks.domain.database
+
+
+import kotlinx.coroutines.flow.Flow
+import ru.vafeen.whisperoftasks.domain.domain_models.Reminder
+
+/**
+ * "Abstract" class for manipulations with database
+ */
+interface ReminderRepository {
+
+    fun getAllRemindersAsFlow(): Flow<List<Reminder>>
+    fun getReminderByIdOfReminder(idOfReminder: Int): Reminder?
+
+
+    /**
+     * Inserting && Updating in database one or more lessons
+     * @param entity [Set of entities to put in database]
+     */
+    suspend fun insertAllReminders(vararg entity: Reminder)
+
+    /**
+     * Deleting from database one or more lessons
+     * @param entity [Set of entities to remove from database]
+     */
+    suspend fun deleteAllReminders(vararg entity: Reminder)
+
+
+    /**
+     * Updating in database one or more lessons
+     * @param entity [Set of entities to update in database]
+     */
+    suspend fun updateAllReminders(vararg entity: Reminder)
+
+}
