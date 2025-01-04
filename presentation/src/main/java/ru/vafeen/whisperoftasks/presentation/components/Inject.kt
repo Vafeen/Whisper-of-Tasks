a5b314b515ec1811a74c7df7d355807765ad4641
Vafeen <666av6@gmail.com>
2025-01-04 18:59:10 +0300

    Add inject

---
+++ presentation/src/main/java/ru/vafeen/whisperoftasks/presentation/components/Inject.kt
@@ -0,0 +1,9 @@
+package ru.vafeen.whisperoftasks.presentation.components
+
+import androidx.compose.runtime.Composable
+import org.koin.core.context.GlobalContext
+import org.koin.compose.getKoin as KoinCompose
+
+@Composable
+inline fun <reified T : Any> InjectCompose(): T = KoinCompose().get()
+inline fun <reified T : Any> Inject(): T = GlobalContext.get().get<T>()
