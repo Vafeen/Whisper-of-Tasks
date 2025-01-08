26b58c2912d0e0b16cc2e2330492f61534191661
Vafeen <666av6@gmail.com>
2025-01-08 22:38:50 +0300

    Fix build.gradle.kts

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "ru.vafeen.whisperoftasks"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "ru.vafeen.whisperoftasks"
+        applicationId = "ru.vafeen.whisperoftasks.tests"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -36,11 +37,8 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(project(":presentation"))
+    implementation(project(":data"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
