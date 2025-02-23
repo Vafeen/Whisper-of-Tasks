f64e9958df1d79aac25f748e693b9fd6727229ca
Vafeen <666av6@gmail.com>
2025-02-23 22:54:46 +0300

    Add navigating to TrashBinScreen

---
+++ presentation/src/main/java/ru/vafeen/whisperoftasks/presentation/components/trash_bin_screen/TrashBinScreen.kt
@@ -0,0 +1,9 @@
+package ru.vafeen.whisperoftasks.presentation.components.trash_bin_screen
+
+import androidx.compose.runtime.Composable
+import ru.vafeen.whisperoftasks.presentation.components.navigation.BottomBarNavigator
+
+@Composable
+internal fun TrashBinScreen(bottomBarNavigator: BottomBarNavigator) {
+
+}
