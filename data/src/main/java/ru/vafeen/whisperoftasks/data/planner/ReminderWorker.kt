372c242275cdb3a0c08d1e67c7f7c383c42fd105
Vafeen <666av6@gmail.com>
2025-03-01 14:05:45 +0300

    Add constraints

--- data/src/main/java/ru/vafeen/whisperoftasks/data/planner/ReminderWorker.kt
+++ data/src/main/java/ru/vafeen/whisperoftasks/data/planner/ReminderWorker.kt
@@ -2,9 +2,13 @@ package ru.vafeen.whisperoftasks.data.planner
 
 
 import android.content.Context
+import androidx.work.Constraints
 import androidx.work.CoroutineWorker
 import androidx.work.Data
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OutOfQuotaPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkerParameters
 import org.koin.core.component.KoinComponent
@@ -52,15 +56,29 @@ internal class ReminderWorker(
         fun oneTimeRequestWithData(reminder: Reminder) =
             OneTimeWorkRequestBuilder<ReminderWorker>()
                 .setInputData(reminder.workerData())
+                .setConstraints(createConstraints())
+                .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
                 .setInitialDelay(calculateDelayToReminder(reminder.dt), TimeUnit.MILLISECONDS)
                 .build()
 
         fun periodicRequestWithData(reminder: Reminder) =
             PeriodicWorkRequestBuilder<ReminderWorker>(reminder.repeatDuration.duration)
                 .setInputData(reminder.workerData())
+                .setConstraints(createConstraints())
+                .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
                 .setInitialDelay(calculateDelayToReminder(reminder.dt), TimeUnit.MILLISECONDS)
                 .build()
-
-
+        /**
+         * Создает ограничения для задач WorkManager.
+         *
+         * @return Объект Constraints с установленными ограничениями для выполнения задач.
+         */
+        private fun createConstraints(): Constraints {
+            return Constraints.Builder()
+                .setRequiresBatteryNotLow(false) // Разрешить выполнение при низком заряде батареи.
+                .setRequiresDeviceIdle(false) // Разрешить выполнение, даже если устройство не в состоянии покоя.
+                .setRequiredNetworkType(NetworkType.NOT_REQUIRED) // Не требовать подключения к сети.
+                .build()
+        }
     }
 }
