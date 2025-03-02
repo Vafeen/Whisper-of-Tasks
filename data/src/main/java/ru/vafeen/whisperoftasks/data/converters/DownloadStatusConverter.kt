88f3b014c73172c4c654ce1d13492b6ab4e4a9ae
Vafeen <666av6@gmail.com>
2025-03-02 04:23:05 +0300

    make some converters simple

--- data/src/main/java/ru/vafeen/whisperoftasks/data/converters/DownloadStatusConverter.kt
+++ data/src/main/java/ru/vafeen/whisperoftasks/data/converters/DownloadStatusConverter.kt
@@ -1,35 +1,14 @@
 package ru.vafeen.whisperoftasks.data.converters
 
-import ru.vafeen.universityschedule.domain.converter.TwoWayBaseConverter
 import ru.vafeen.whisperoftasks.domain.network.result.DownloadStatus
 import android.vafeen.direct_refresher.downloader.DownloadStatus as LibDownloadStatus
 
 /**
  * Конвертер для преобразования статуса загрузки между библиотечным типом и типом приложения.
- * Реализует двусторонний конвертер, но в настоящее время конвертация из DownloadStatus в LibDownloadStatus не поддерживается.
  */
-internal class DownloadStatusConverter : TwoWayBaseConverter<LibDownloadStatus, DownloadStatus> {
-
-    /**
-     * Преобразует статус загрузки из библиотечного типа в тип приложения.
-     *
-     * @param a Статус загрузки из библиотеки.
-     * @return Соответствующий статус загрузки для приложения.
-     */
-    override fun convertAB(a: LibDownloadStatus): DownloadStatus = when (a) {
-        is LibDownloadStatus.Error -> DownloadStatus.Error(a.exception)
-        is LibDownloadStatus.InProgress -> DownloadStatus.InProgress(a.percentage)
-        LibDownloadStatus.Started -> DownloadStatus.Started
-        LibDownloadStatus.Success -> DownloadStatus.Success
-    }
-
-    /**
-     * Преобразует статус загрузки из типа приложения в библиотечный тип.
-     * В настоящее время не поддерживается и выбрасывает исключение.
-     *
-     * @param b Статус загрузки из приложения.
-     * @throws Exception Выбрасывается всегда, поскольку конвертация не реализована.
-     */
-    override fun convertBA(b: DownloadStatus): LibDownloadStatus =
-        throw Exception("ru.vafeen.universityschedule.data.converters.DownloadStatusConverter is not defined")
+internal fun LibDownloadStatus.toDownloadStatus(): DownloadStatus = when (this) {
+    is LibDownloadStatus.Error -> DownloadStatus.Error(this.exception)
+    is LibDownloadStatus.InProgress -> DownloadStatus.InProgress(this.percentage)
+    LibDownloadStatus.Started -> DownloadStatus.Started
+    LibDownloadStatus.Success -> DownloadStatus.Success
 }
--- data/src/main/java/ru/vafeen/whisperoftasks/data/converters/ReleaseConverter.kt
+++ data/src/main/java/ru/vafeen/whisperoftasks/data/converters/ReleaseConverter.kt
@@ -1,7 +1,6 @@
 package ru.vafeen.whisperoftasks.data.converters
 
 import ru.vafeen.universityschedule.data.network.dto.github_service.ReleaseDTO
-import ru.vafeen.universityschedule.domain.converter.TwoWayBaseConverter
 import ru.vafeen.whisperoftasks.domain.domain_models.Release
 
 /**
@@ -9,38 +8,17 @@ import ru.vafeen.whisperoftasks.domain.domain_models.Release
  *
  * Данный конвертер предназначен для преобразования данных о релизах,
  * полученных с сервера, в доменную модель, используемую в приложении.
- * Обратное преобразование не реализовано, так как это не предусмотрено логикой приложения.
  */
-internal class ReleaseConverter : TwoWayBaseConverter<ReleaseDTO?, Release?> {
-
-    /**
-     * Преобразует [ReleaseDTO] в [Release].
-     *
-     * @param a Экземпляр [ReleaseDTO], полученный из сетевого слоя.
-     * @return Экземпляр [Release], используемый в доменном уровне приложения.
-     */
-    override fun convertAB(a: ReleaseDTO?): Release? {
-        val apk = a?.assets?.find {
-            it.browserDownloadUrl.contains(".apk")
-        }
-        return if (a != null && apk != null) {
-            Release(
-                tagName = a.tagName,
-                apkUrl = apk.browserDownloadUrl,
-                size = apk.size,
-                body = a.body
-            )
-        } else null
+internal fun ReleaseDTO.toRelease(): Release? {
+    val apk = this.assets.find {
+        it.browserDownloadUrl.contains(".apk")
     }
-
-    /**
-     * Преобразование [Release] в [ReleaseDTO] не реализовано.
-     *
-     * @param b Экземпляр [Release].
-     * @throws Exception Всегда выбрасывает исключение, так как обратное преобразование
-     * недоступно из-за особенностей логики приложения.
-     */
-    override fun convertBA(b: Release?): ReleaseDTO? {
-        throw Exception("ru.vafeen.universityschedule.domain.converters.ReleaseConverterImpl::convertBA body not yet implemented")
-    }
+    return if (apk != null) {
+        Release(
+            tagName = this.tagName,
+            apkUrl = apk.browserDownloadUrl,
+            size = apk.size,
+            body = this.body
+        )
+    } else null
 }
